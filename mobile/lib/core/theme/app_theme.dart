fcd898262cab13d3ab3da0e2101c1cedc2246c5e
Mehbinamal <mehbinamal6@gmail.com>
2026-06-29 11:23:21 +0530

    feat(frontend): initialize Flutter project structure and app foundation

---
+++ mobile/lib/core/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+
+    scaffoldBackgroundColor: AppColors.background,
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+    ),
+
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      centerTitle: false,
+    ),
+
+    cardTheme: CardThemeData(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+    ),
+  );
+}
